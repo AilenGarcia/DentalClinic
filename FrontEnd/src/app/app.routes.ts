a6a643afc00f0b55a5d4a7b5b1ee43d56727c14c
g4st1xxx <gastonignaciodosmil@gmail.com>
2025-10-21 18:54:43 -0300

    Generación de componentes principales y routeo.
    Se maquetó el Header.
    Se generaron las principales vistas del proyecto.
    Se agregó routeo para vincular el Navbar del Header con los componentes correspondientes.

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Turnos } from './turnos/turnos';
+import { Register } from './register/register';
+import { Login } from './login/login';
+import { Odontologos } from './odontologos/odontologos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: 'home',
+        component: Home,
+        title: 'Dental Clinic'
+    },
+    {
+        path: 'turnos',
+        component: Turnos,
+        title: 'Agendar turno'
+    },
+    {
+        path: 'odontologos',
+        component: Odontologos,
+        title: 'Listado de odontólogos'
+    },
+    {
+        path: 'register',
+        component: Register,
+        title: 'Registrar nuevo usuario'
+    },
+    {
+        path: 'login',
+        component: Login,
+        title: 'Iniciar sesión'
+    }
+];
