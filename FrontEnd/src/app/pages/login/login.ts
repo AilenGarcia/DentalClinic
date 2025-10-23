d958d5986d5c9071a18fb828a38a57bfe36541e3
AilenGarcia <ailenoicor15@gmail.com>
2025-10-23 12:18:02 -0300

    add erros styles and login form logic[

--- FrontEnd/src/app/pages/login/login.ts
+++ FrontEnd/src/app/pages/login/login.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +8,27 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.css'
 })
 export class Login {
+  private readonly formBuilder = inject(FormBuilder)
 
+  protected readonly form = this.formBuilder.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  get email(){
+    return this.form.controls.email;
+  }
+
+  get password(){
+    return this.form.controls.password;
+  }
+
+  handleSubmit(){
+    if(confirm('¿Desea confirmar los datos?')){
+      const user = this.form.getRawValue();
+      console.log(user);
+    }
+  }
 }
+
+
